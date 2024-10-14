a66e3e454ff731c9672b6774272d198484924bc7
xu rui <xurui1@pjlab.org.cn>
2024-10-14 14:37:45 +0800

    feat: try auto build

--- flaskr/db.py
+++ flaskr/db.py
@@ -9,6 +9,7 @@ def get_db():
     """Connect to the application's configured database. The connection
     is unique for each request and will be reused if this is called
     again.
+    Let us try it!
     """
     if "db" not in g:
         g.db = sqlite3.connect(
